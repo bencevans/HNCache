77edad4066d078b9ff15867daf87e41e5f2b7f08
Ben Evans <ben@bensbit.co.uk>
2013-08-27 00:47:50 +0100

    redis rtg

--- redis.js
+++ redis.js
@@ -10,20 +10,20 @@ var config = require('./config.js');
  * Redis Client
  */
 
+var host = process.env.REDIS_HOST || null;
+var port = process.env.REDIS_PORT || null;
 var options = {};
 
 if(typeof process.env.REDIS_AUTH !== "undefined") {
-  options.auth_pass = rtg.auth.split(":")[1];
+  options.auth_pass = process.env.REDIS_AUTH;
 } else if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  host = rtg.hostname;
+  port = rtg.port;
   options.auth_pass = rtg.auth.split(":")[1];
 }
 
-if (process.env.REDISTOGO_URL) {
-  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname, options);
-} else {
-  var redis = require('redis').createClient(process.env.REDIS_PORT || null, process.env.REDIS_HOST || null, options);
-}
+var redis = require('redis').createClient(port, host, options);
 
 /**
  * Exports
